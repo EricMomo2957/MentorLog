47f706a6d572f4dab844cfee8c9b2cf20936106f
EricMomo2957 <Momoe2957@gmail.com>
2026-03-20 06:01:33 +0800

    Modified package json

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import pool from '../config/db';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 export const login = async (req: Request, res: Response) => {
