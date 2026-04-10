e8e1683b36c3c3e1be4eff2f4b70a9fc2d0b733c
EricMomo2957 <Momoe2957@gmail.com>
2026-04-11 02:04:28 +0800

    Update App.tsx with adding for AdminLayout

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -15,7 +15,7 @@ import AdminProfile from './pages/admin/AdminProfile';
 import AdminCalendar from './pages/admin/AdminCalendar';
 import ManageRequest from './pages/admin/ManageRequest';
 import ManageFeedback from './pages/admin/ManageFeedback'; // Import the new component
-//import ManageAnnouncement from './pages/admin/ManageAnnouncement';
+import ManageAnnouncement from './pages/admin/ManageAnnouncement';
  // Import the new component
 
 // Student Imports
@@ -27,6 +27,7 @@ import StudentSettings from './pages/student/StudentSettings';
 import StudentCalendar from './pages/student/StudentCalendar';
 import StudentRequest from './pages/student/StudentRequest'; // Import the new component
 import StudentFeedback from './pages/student/StudentFeedback'; // Import the new component
+
 
 function App() {
   return (
@@ -49,7 +50,7 @@ function App() {
           { path: "/admin-profile", element: <AdminProfile /> }, 
           { path: "/admin-settings", element: <AdminSettings /> },
           { path: "/manage-feedback", element: <ManageFeedback /> },
-          //{ path: "/manage-announcements", element: <ManageAnnouncement /> }
+          { path: "/manage-announcements", element: <ManageAnnouncement /> }
         ].map((route) => (
           <Route 
             key={route.path}
